4cf2609e2e4902088ab27743c48557b9d7af95f0
Sabbir <sabbirrahman42@iut-dhaka.edu>
2021-09-28 20:39:06 +0600

    counter and current value counter

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,43 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0)
+
+  const complexIncrease = () => {
+    setTimeout(() => {
+      //setValue(value+1)
+      //take the current value for prebious 10 click at a time will update for once but this time it will update 10 times
+      setValue((prevState) => {
+        return prevState + 1;
+      })
+    },2000)
+  }
+  return (
+    <>
+      <section style={{ margin: "4 rem 0" }}>
+        <h2>Regular Counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={() => setValue(value - 1)}>
+          decrease
+        </button>
+        <button className="btn" onClick={() => setValue(0)}>
+          reset
+        </button>
+        <button className="btn" onClick={() => setValue(value + 1)}>
+          increase
+        </button>
+      </section>
+      
+      <section style={{ margin: "4 rem 0" }}>
+        <h2>Regular Counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={complexIncrease}>
+          Increase after 2 seconds
+        </button>
+        
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
