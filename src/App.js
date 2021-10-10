afe35928e2cf2abce8f6e0cf90e2d18328e127bd
sabbir <sabbirrahman42@iut-dhaka.edu>
2021-10-10 22:35:53 +0600

    multiple render, loading before fetching, error throw

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Setup from './tutorial/2-useEffect/setup/2-useEffect-cleanup'
+import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns'
 
 function App() {
   return (
     <div className='container'>
-      <Setup/>
+      <Setup />
     </div>
   )
 }
