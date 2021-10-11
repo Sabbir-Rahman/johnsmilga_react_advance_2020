6ee5fa254ab201c6439cd5bd6b71ea0511fe3ce1
sabbir <sabbirrahman42@iut-dhaka.edu>
2021-10-11 15:22:19 +0600

    show/hide on condition toggle button with cleanup function

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,36 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
+  const [show,setShow] = useState(false)
+  return <>
+    <button className='btn' onClick={() => setShow(!show)}>
+      show/hide
+    </button>
+    {show && <Item/>}
+  </>
   return <h2>show/hide</h2>;
 };
+
+const Item = () => {
+  const [size,setSize] = useState(window.innerWidth);
+  const checkSize = () => {
+    setSize(window.innerWidth)
+  }
+
+  useEffect(()=> {
+    window.addEventListener('resize', checkSize)
+
+    //cleanup is important
+    return ()=> {
+      window.removeEventListener('resize',checkSize)
+    }
+  },[])
+  return (
+    <div style={{ marginTop: '2rem'}}>
+      <h1>window</h1>
+      <h2>size : {size} px</h2>
+    </div>
+  )
+}
 
 export default ShowHide;
