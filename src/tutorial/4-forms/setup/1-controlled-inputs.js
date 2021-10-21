58c43291e8505cb9f180691b8196268e7c534ea3
sabbir <sabbirrahman42@iut-dhaka.edu>
2021-10-22 04:48:48 +0600

    form basics

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,29 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  // browser default behaviour will try to auto submit the form and reload that's why we need to prevent default
+  const handleSubmit = (e) => {
+    
+    e.preventDefault();
+    console.log('Hello world')
+  }
+  return (
+  <>
+  <article>
+    <form className="form" onSubmit={handleSubmit}>
+      <div className="form-control">
+        <label htmlFor="firstName">Name : </label>
+        <input type="text" id='firstName' name='firstName'/>
+      </div>
+      <div className="form-control">
+        <label htmlFor="email">Email : </label>
+        <input type="text" id='email' name='email'/>
+      </div>
+      <button type="submit" onClick={handleSubmit}>Add person</button>
+    </form>
+  </article>
+  </>
+  );
 };
 
 export default ControlledInputs;
