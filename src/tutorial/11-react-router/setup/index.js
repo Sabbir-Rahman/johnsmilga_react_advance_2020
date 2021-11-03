5215fd1b8c6a9ca5791e6395aa70000554959945
sabbir <sabbirrahman42@iut-dhaka.edu>
2021-11-03 23:12:55 +0600

    react router with dynamic routing, error,param routing

--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -12,21 +12,23 @@ import Navbar from './Navbar';
 const ReactRouterSetup = () => {
   return (
   <Router>
-    <Route exact path="/">
-      <Home/>
-    </Route>
-    <Route path="/about">
-      <About/>
-    </Route>
-    <Route path="/people">
-      <People/>
-    </Route>
-    {/* <Route path="/error">
-      <Error/>
-    </Route>
-    <Route path="/person">
-      <Person/>
-    </Route> */}
+    <Navbar/>
+    <Switch>
+      <Route exact path="/">
+        <Home/>
+      </Route>
+      <Route path="/about">
+        <About/>
+      </Route>
+      <Route path="/people">
+        <People/>
+      </Route>
+      <Route path="/person/:id" children={<Person/>}></Route>
+      <Route path="*">
+        <Error/>
+      </Route>
+      
+    </Switch>
   </Router>
   )
 };
